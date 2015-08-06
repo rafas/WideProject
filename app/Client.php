315cd5b39f38042bbea4089b33a0a6097f0f5eb5
Rafael Santos <rafael@widenet.com.br>
2015-08-06 17:55:02 -0300

    Client Model e Migration adicionados

---
+++ app/Client.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace WideProject;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $fillable = [
+        'name',
+        'responsible',
+        'email',
+        'phone',
+        'address',
+        'obs'
+    ];
+
+    //
+}
